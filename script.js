6b6db9969b4fcbf6038d83a2f98bd899c49d1b38
Mahmoud_Gad <hooda.gad333@gmail.com>
2025-01-22 20:51:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const ball = document.getElementById("ball");
+const box = document.getElementById("box");
+
+box.addEventListener("click", (e) => {
+  const ball_rect = ball.getBoundingClientRect();
+  const x_pos =
+    e.clientX - box.offsetLeft - box.offsetWidth / 2 - ball_rect.width / 2;
+  const y_pos =
+    e.clientY - box.offsetTop - box.offsetHeight / 2 - ball_rect.height / 2;
+  ball.style.transform = "translate(" + x_pos + "px," + y_pos + "px)";
+  console.log("x:", e.clientX, "y:", e.clientY);
+});
